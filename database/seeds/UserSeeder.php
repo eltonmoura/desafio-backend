1ccb3af4c683d6ee93b18e28edb6436b44dd381f
eltonmoura <elton.ms@gmail.com>
2020-08-14 14:42:35 -0300

    add users

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -13,12 +13,44 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        $user = User::create([
+        User::create([
             'name' => 'Admin',
             'email' => 'admin@mail.com',
             'password' => Hash::make('senha123'),
-            'identity' => '111.111.111-11',
+            'identity' => '111.111.111-01',
             'type' => 'person',
+        ]);
+
+        User::create([
+            'name' => 'João da Silva',
+            'email' => 'joao@mail.com',
+            'password' => Hash::make('senha123'),
+            'identity' => '111.111.111-02',
+            'type' => 'person',
+        ]);
+
+        User::create([
+            'name' => 'Maria Pereira',
+            'email' => 'maria@mail.com',
+            'password' => Hash::make('senha123'),
+            'identity' => '111.111.111-03',
+            'type' => 'person',
+        ]);
+
+        User::create([
+            'name' => 'Padaria Boa',
+            'email' => 'padaria@mail.com',
+            'password' => Hash::make('senha123'),
+            'identity' => '11.111.111/0001-01',
+            'type' => 'company',
+        ]);
+
+        User::create([
+            'name' => 'Mercado Barato',
+            'email' => 'mercado@mail.com',
+            'password' => Hash::make('senha123'),
+            'identity' => '11.111.111/0001-02',
+            'type' => 'company',
         ]);
     }
 }
